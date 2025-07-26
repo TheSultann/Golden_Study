497e9d99a7bc4f7658cda4611fb592b48dd6e03e
TheSultann <otanazarovsultanbek@gmail.com>
2025-07-26 11:46:51 +0500

    fix api.js

---
+++ src/api.js
@@ -0,0 +1,17 @@
+// src/api.js
+
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL
+});
+
+API.interceptors.request.use((req) => {
+    const token = localStorage.getItem('userToken');
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+});
+
+export default API;
