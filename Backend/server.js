620e23f45b1ab585410d7e719df8ee62e67d10af
TheSultann <otanazarovsultan@gmail.com>
2026-05-18 15:03:24 +0500

    fix: Render start command - run server.js from project root

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
+const path = require('path');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const app = require('./app');
 const { startInvoiceScheduler } = require('./jobs/invoiceScheduler');
 const { getServerEnv } = require('./config/env');
